816a56fc97f0159cf892801a6520a6051ef7e754
Scott Daily <scott-daily@users.noreply.github.com>
2019-09-13 11:40:34 -0400

    Update and rename doublestack.java to DoubleStack.java

--- java/DoubleStack.java
+++ java/DoubleStack.java
@@ -36,7 +36,7 @@ Sample Output 1:
 
 import java.util.*;
 
-class Doublestack {
+class DoubleStack {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();

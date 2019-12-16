fca2b9537ffbe3ebda8ec5cb3e82ba9962c557c6
Scott Daily <scottjuggles@gmail.com>
2019-12-16 09:35:11 -0500

    update RotatingTables.java

--- java/RotatingTable.java
+++ java/RotatingTable.java
@@ -1,19 +1,44 @@
+/*
+Given a table of integer numbers. You should rotate rows of the table by the specified distance.
+Input data format:
+The first line contains two numbers: a number of rows and a number of columns of the table.
+The following lines describe rows of the table. In each row, all elements are separated by spaces.
+The last line consists of one number - the distance for rotating.
+Sample Input:
+3 3
+1 1 1
+2 2 2
+3 3 3
+1
+Sample Output:
+3 3 3
+1 1 1
+2 2 2
+*/
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class RotatingTable {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int numRows = scanner.nextInt();
         int numCols = scanner.nextInt();
+        scanner.nextLine();
 
-        int[][] table = new int[numRows][numCols];
+        List<String> inputTable = new ArrayList<String>();
 
         for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numCols; j++) {
-                table[i][j] = Integer.parseInt(scanner.next());
-            }
+            inputTable.add(scanner.nextLine());
         }
-        System.out.println(Arrays.deepToString(table));
+
+        int rotations = (0 + scanner.nextInt()) % numRows;
+        Collections.rotate(inputTable,rotations);
+    
+        for (int i = 0; i < numRows; i++) {
+            System.out.print(inputTable.get(i) + " ");
+            System.out.println();
+        }
     }
 }

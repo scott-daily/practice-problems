55a0bf7b0ee5c22d0834cfea66fb9f1cdfb64a61
Scott Daily <scottjuggles@gmail.com>
2020-02-20 09:27:38 -0500

    add inOrderTraversal.cpp

---
+++ c++/inOrderTraversal.cpp
@@ -0,0 +1,26 @@
+// Given a binary tree, return the inorder traversal of its nodes' values.
+
+#include <vector>
+
+class inOrderTraversal {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        std::vector<int> inorderVect;
+        if (root == nullptr) {
+            return inorderVect;
+        }
+        inOrder(root, inorderVect);
+        return inorderVect;
+    }
+    
+    void inOrder(TreeNode* root, std::vector<int>& treeVect) {
+        if (root == nullptr) {
+            return;
+        }
+        else {
+            inOrder(root->left, treeVect);
+            treeVect.push_back(root->val);
+            inOrder(root->right, treeVect);
+        }
+    }
+};

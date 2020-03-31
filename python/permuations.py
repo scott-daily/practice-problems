5a277e0e1b6f5ce181d0e306299346b992503290
Scott Daily <scottjuggles@gmail.com>
2020-03-31 13:17:34 -0400

    add permuations.py

---
+++ python/permuations.py
@@ -0,0 +1,17 @@
+'''How many numbers between 1 and 100 (inclusive) are divisible by 10 or 7?'''
+
+x = 1
+
+def permutations(num1, num2):
+    count = 0
+    for x in range(1, 101):
+        if x%num1 == 0:
+            count = count + 1
+        if x%num2 == 0:
+            count = count + 1
+        if x%num1 == 0 and x%num2 == 0:
+            count = count - 1
+
+    return count
+
+print(permutations(7,10))
